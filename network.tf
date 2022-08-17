ebba20b22608e7a703ffe18acfc37594d34291b2
Scott Deindorfer <scott.deindorfer@bluestem.com>
2022-08-17 20:10:56 +0000

    fix network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/bluestem-scott/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   version = "3.5.0"
 }
